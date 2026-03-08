c8326624c83dc16872a97be541ae4ba115187ae3
winxp <2947602074@qq.com>
2026-03-08 20:31:57 +0800

    feat(api): 添加待办事项相关接口类型定义和请求方法
    添加待办事项列表查询和详情查询的接口类型定义
    实现对应的请求方法封装

---
+++ src/api/todo.ts
@@ -0,0 +1,67 @@
+import { http } from "@/utils/http";
+
+type ClientTodoVO = {
+  id: number;
+  userId: number;
+  title: string;
+  content: string;
+  startTime: string;
+  endTime: string;
+  status: number;
+  isTop: number;
+};
+
+type TagDetails = {
+  id: number;
+  userId: number;
+  name: string;
+  color: string;
+  sortOrder: number;
+};
+
+type ClientTodoDetails = {
+  id: number;
+  userId: number;
+  title: string;
+  content: string;
+  startTime: string;
+  endTime: string;
+  status: string;
+  isTop: string;
+  categoryName: string;
+  tags: TagDetails[];
+};
+
+type PageBean<T> = {
+  pageNum: number;
+  pageSize: number;
+  pages: number;
+  total: number;
+  records: T[];
+};
+
+type ClientTodoQuery = {
+  pageNum?: number;
+  pageSize?: number;
+  userId?: number;
+  title?: string;
+  categoryId?: number;
+  priority?: number;
+  status?: number;
+  isTop?: number;
+  tagIdList?: number[];
+};
+
+type Result<T> = {
+  code: number;
+  msg: string;
+  data: T;
+};
+
+export const getTodoList = (params: ClientTodoQuery) => {
+  return http.request<Result<PageBean<ClientTodoVO>>>("post", "/api/todo/list", { data: params });
+};
+
+export const getTodoDetails = (id: number) => {
+  return http.request<Result<ClientTodoDetails>>("get", `/api/todo/details/${id}`);
+};
