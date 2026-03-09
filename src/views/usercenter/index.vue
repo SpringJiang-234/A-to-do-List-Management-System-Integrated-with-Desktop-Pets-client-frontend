fbd5d3ce6b24bdc864833332ca65309e738724ce
winxp <2947602074@qq.com>
2026-03-10 03:35:28 +0800

    feat(用户中心): 优化用户信息编辑界面交互
    - 为头像、昵称、性别、生日字段添加编辑提示工具条
    - 将编辑按钮改为开关切换模式
    - 移除退出登录按钮，保留修改密码和注销账户功能
    - 调整表单布局和样式

--- src/views/usercenter/index.vue
+++ src/views/usercenter/index.vue
@@ -57,65 +57,46 @@ onMounted(() => {
         </div>
       </template>
       <div class="avatar-wrapper">
-        <el-avatar size="large" :src="avatar" />
+        <el-tooltip :disabled="!editable" class="box-item" effect="dark" content="点击修改头像" placement="top">
+          <el-avatar size="large" :src="avatar" />
+        </el-tooltip>
       </div>
       <div class="form-item">
-        <el-input
-          v-model="nickname"
-          :disabled="!editable"
-          style="width: 240px"
-          placeholder="输入新昵称"
-        />
+        <el-tooltip :disabled="!editable" class="box-item" effect="dark" content="不可修改邮箱账号" placement="top">
+          <el-input v-model="email" style="width: 240px" placeholder="邮箱账号" disabled />
+        </el-tooltip>
       </div>
       <div class="form-item">
-        <el-input
-          v-model="email"
-          style="width: 240px"
-          placeholder="邮箱账号"
-          disabled
-        />
+        <el-tooltip :disabled="!editable" class="box-item" effect="dark" content="可修改昵称" placement="top">
+          <el-input v-model="nickname" :disabled="!editable" style="width: 240px" placeholder="输入新昵称" />
+        </el-tooltip>
       </div>
       <div class="form-item">
-        <el-select
-          v-model="gender"
-          :disabled="!editable"
-          style="width: 240px"
-          placeholder="选择性别"
-        >
-          <el-option label="男" value="1" />
-          <el-option label="女" value="2" />
-          <el-option label="未知" value="3" />
-        </el-select>
+        <el-tooltip :disabled="!editable" class="box-item" effect="dark" content="可修改性别" placement="top">
+          <el-select v-model="gender" :disabled="!editable" style="width: 240px" placeholder="选择性别">
+            <el-option label="男" value="1" />
+            <el-option label="女" value="2" />
+            <el-option label="未知" value="3" />
+          </el-select>
+        </el-tooltip>
       </div>
       <div class="form-item">
-        <!-- 选择日期必须小于等于今天 -->
-        <el-date-picker
-          v-model="birthday"
-          :disabled="!editable"
-          type="date"
-          value-format="yyyy-MM-dd"
-          style="width: 240px"
-          placeholder="选择生日"
-        />
+        <el-tooltip :disabled="!editable" class="box-item" effect="dark" content="可修改生日" placement="top">
+          <div>
+            <el-date-picker v-model="birthday" :disabled="!editable" type="date" value-format="yyyy-MM-dd"
+              style="width: 240px" placeholder="选择生日" />
+          </div>
+        </el-tooltip>
       </div>
       <div class="form-item">
-        <el-button type="primary" style="width: 115px" @click="handleEdit"
-          >修改</el-button
-        >
-        <Dialog
-          buttonText="保存"
-          title="保存"
-          content="确认要保存修改吗？"
-          closeConfirmMessage="确认关闭弹窗吗？"
-          confirmButtonType="primary"
-          buttonWidth="115px"
-          buttonType="primary"
-          @confirm="handleSaveConfirm"
-        />
+        <el-switch v-model="editable" active-text="修改" inactive-text="保存" />
       </div>
       <div class="form-item">
-        <el-button type="primary" style="width: 115px">退出登录</el-button>
-        <el-button type="danger" style="width: 115px">注销账户</el-button>
+        <Dialog buttonText="修改密码" title="修改密码" content="确认要修改密码吗？" closeConfirmMessage="确认关闭弹窗吗？"
+          confirmButtonType="primary" buttonType="primary" @confirm="handleSaveConfirm" />
+      </div>
+      <div class="form-item">
+        <el-button type="danger">注销账户</el-button>
       </div>
     </el-card>
   </div>
