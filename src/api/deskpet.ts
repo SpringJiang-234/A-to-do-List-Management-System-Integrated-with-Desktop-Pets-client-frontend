8ef9201a9b3212e4b82587884e6c442d39f0616e
winxp <2947602074@qq.com>
2026-03-11 04:56:31 +0800

    feat(api): 添加桌面宠物相关API接口
    新增获取桌面宠物信息和更新宠物信息的API接口及类型定义

---
+++ src/api/deskpet.ts
@@ -0,0 +1,41 @@
+import { http } from "@/utils/http";
+
+export type DesktopPetResult = {
+  code: number;
+  msg: string;
+  data: {
+    id: number;
+    userId: number;
+    nickname: string;
+    energy: number;
+    mood: number;
+    intimacy: number;
+    exp: number;
+    level: number;
+    createTime: string;
+    updateTime: string;
+  } | null;
+};
+
+export type UpdateDesktopPetResult = {
+  code: number;
+  msg: string;
+  data: null;
+};
+
+export type DesktopPetDTO = {
+  nickname?: string;
+  energy?: number;
+  mood?: number;
+  intimacy?: number;
+  exp?: number;
+  level?: number;
+};
+
+export const getDesktopPetInfo = () => {
+  return http.request<DesktopPetResult>("get", "/api/client/desktopPet/info");
+};
+
+export const updateDesktopPet = (data?: DesktopPetDTO) => {
+  return http.request<UpdateDesktopPetResult>("post", "/api/client/desktopPet/update", { data });
+};
